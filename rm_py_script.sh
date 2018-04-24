8db3645d503c93f37cd399512bdb8bc85d4dd091
David Victor Dylus <ucbpdvd@login07.data.legion.ucl.ac.uk>
2018-04-24 07:15:16 +0100

    removed .sh files

--- rm_py_script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#$ -l mem=4G
-#$ -S /bin/bash
-#$ -l h_rt=0:10:0
-#$ -pe smp 1
-#$ -j y
-#$ -N rm_FALSP
-#$ -wd /home/ucbpdvd/Scratch/output
-rm -r /home/ucbpdvd/Scratch/avian/reads/FALSP
