6380d80eae59c026788ca9daa553bb30ec2edd29
dvdylus <daviddylus@gmail.com>
2018-04-26 13:41:12 +0200

    addition of tmp folder for parallel-fastq-dump to current folder

--- scripts/lsf_submit.py
+++ scripts/lsf_submit.py
@@ -105,7 +105,7 @@ echo 'Created read $speciesid'
 cd /scratch/beegfs/weekly/ddylus/avian/reads/$speciesid
 ascp -v -QT -k1 -l100M -i /software/Utility/aspera_connect/3.7.4.147727/etc/asperaweb_id_dsa.openssh anonftp@ftp.ncbi.nlm.nih.gov:/sra/sra-instant/reads/ByRun/sra/SRR/${srr:0:6}/$srr/$srr.sra ./
 echo 'Finished download'
-parallel-fastq-dump -s *.sra -t 4 -O . --split-files
+parallel-fastq-dump -s *.sra -t 4 -O . --split-files --tmpdir .
 echo 'Finished getting fastq from sra and split files'
 mv *\_1.fastq $speciesid\_1.fq
 mv *\_2.fastq $speciesid\_2.fq
@@ -129,7 +129,7 @@ echo 'Created read $speciesid'
 cd /scratch/beegfs/weekly/ddylus/avian/reads/$speciesid
 ascp -v -QT -k1 -l100M -i /software/Utility/aspera_connect/3.7.4.147727/etc/asperaweb_id_dsa.openssh anonftp@ftp.ncbi.nlm.nih.gov:/sra/sra-instant/reads/ByRun/sra/SRR/${srr:0:6}/$srr/$srr.sra ./
 echo 'Finished download'
-parallel-fastq-dump -s *.sra -t 4 -O .
+parallel-fastq-dump -s *.sra -t 4 -O . --tmpdir .
 echo 'Finished getting fastq from sra'
 mv *.fastq $speciesid\_1.fq
 echo 'Finished moving files'""" % (species_id, '%', species_id, '%', species_id, sra, species_id)
