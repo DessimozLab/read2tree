866a14c6cde2436988e190f814a22b90ac67f7f0
David Dylus <ddylus@dee-serv06.vital-it.ch>
2018-10-31 13:22:37 +0100

    minor changes

--- read2tree/Mapper.py
+++ read2tree/Mapper.py
@@ -123,11 +123,11 @@ class Mapper(object):
                 self._reads = self.args.reads[0]
                 self._species_name = self._reads.split("/")[-1].split(".")[0]
 
-        if self.args.species_name:
-            self._species_name = self.args.species_name
+        #if self.args.species_name:
+        #    self._species_name = self.args.species_name
 
-        if species_name:
-            self._species_name = species_name
+        #if species_name:
+        #    self._species_name = species_name
 
         # load pyopa related stuff
         # self.defaults = pyopa.load_default_environments()
