174855783ea69850853c8b058b99f04daa853fba
dvdylus <daviddylus@gmail.com>
2019-03-06 09:57:08 +0100

    better script to sample reads

--- scripts/sample_reads.py
+++ scripts/sample_reads.py
@@ -3,7 +3,16 @@ import os, shutil, time, random, tempfile
 import numpy as np
 from read2tree.FastxReader import FastxReader
 from math import ceil
+import logging, yaml
+from pkg_resources import resource_string
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+conf = resource_string(__name__, '../read2tree/logging/log.yaml')
 
+D = yaml.load(conf)
+D.setdefault('version', 1)
+logging.config.dictConfig(D)
+
+logger = logging.getLogger(__name__)
 
 def sample_from_reads(reads, genome_len, coverage):
     '''
@@ -24,7 +33,7 @@ def sample_from_reads(reads, genome_len, coverage):
         if idx_random:
             sampled_reads.append(_sample_read_file(reads[0], idx_random))
             sampled_reads.append(_sample_read_file(reads[1], idx_random))
-            print('Reads can be found at: {}'.format(sampled_reads))
+            logger.info('Reads can be found at: {}'.format(sampled_reads))
             shutil.copy(sampled_reads[0], reads[0].split(".")[0]+".fa")
             shutil.copy(sampled_reads[1], reads[1].split(".")[0]+".fa")
         else:
@@ -32,26 +41,28 @@ def sample_from_reads(reads, genome_len, coverage):
 
     end = time.time()
     elapsed_time = end - start
-    print('Sampling of reads took {}.'.format(elapsed_time))
+    logger.info('Sampling of reads took {}.'.format(elapsed_time))
 
     return sampled_reads
 
 
 def _get_num_reads_by_coverage(file, num_records, genome_len, coverage):
     read_len = _get_read_len(file, num_records)
-    print('Average read length estimated to {}.'.format(read_len))
+    logger.info('Average read length estimated to {}.'.format(read_len))
     return int(ceil(genome_len * coverage / (2 * read_len)))
 
 
 def _get_vector_random_reads(file, genome_len, coverage):
     total_records = _get_num_reads(file)
     num_reads_by_coverage = _get_num_reads_by_coverage(file, total_records, genome_len, coverage)
-    print('Sampling {} / {} reads for {}X coverage.'.format(num_reads_by_coverage, total_records, coverage))
+    logger.info('Sampling {} / {} reads for {}X coverage.'.format(num_reads_by_coverage, total_records, coverage))
     if num_reads_by_coverage > total_records:
-        print("Not enough reads available for sampling, using them all.")
+        logger.info("Not enough reads available for sampling, using them all.")
         return None
     else:
-        return sorted(list(set(random.sample(range(total_records + 1), num_reads_by_coverage))))
+        x = np.sort(np.random.choice((total_records + 1), num_reads_by_coverage, replace=False))
+        return list(x)
+        #old: return sorted(list(set(random.sample(range(total_records + 1), num_reads_by_coverage))))
 
 
 def _get_num_reads(file):
@@ -64,10 +75,10 @@ def _get_num_reads(file):
 
 def _get_read_len(file, num_records):
     fastq_reader = FastxReader(file)
-    if num_records < 100000:
+    if num_records < 10000:
         samples = num_records
     else:
-        samples = 100000
+        samples = 10000
     lens = [0] * samples
     with fastq_reader.open_fastx() as f:
         for i, l in enumerate(fastq_reader.readfq(f)):
@@ -77,7 +88,7 @@ def _get_read_len(file, num_records):
                 break
         mean_len = np.mean(lens)
         median_len = np.median(lens)
-    print('The reads have a mean length of {} '
+    logger.info('The reads have a mean length of {} '
           'and a median length of {}.'
           .format(mean_len, median_len))
     return mean_len
@@ -119,17 +130,17 @@ def _sample_read_file(file, select_idx):
             initial_length = initial_length + len(seq)
             try:
                 if i == select_idx[k]:
-                    # print(name)
+                    # logger.info(name)
                     seq_record_str = _get_2_line_fasta_string(name, seq)
                     out_file.write(seq_record_str)
                     sampling_length = sampling_length + len(seq)
                     k = k + 1
             except IndexError as e:
-                print('i={}, k={}, len={} and last select_idx={} has error:{}'.format(i,k,len(select_idx),select_idx[-1],e))
+                logger.info('i={}, k={}, len={} and last select_idx={} has error:{}'.format(i,k,len(select_idx),select_idx[-1],e))
                 pass
             if k == len(select_idx):
                 break
-    print('Cummulative length of all reads {}bp. Cummulative length of sampled reads {}bp'.format( initial_length,sampling_length))
+    logger.info('Cummulative length of all reads {}bp. Cummulative length of sampled reads {}bp'.format( initial_length,sampling_length))
     out_file.close()
     return out_file.name
 
@@ -151,5 +162,5 @@ if __name__ == "__main__":
                             'end add separated by space.')
 
     conf = parser.parse_args()
-
+    logger.info('----- WE ARE SAMPLING THE READS -----')
     sample_from_reads(conf.reads, conf.genome_len, conf.coverage)
