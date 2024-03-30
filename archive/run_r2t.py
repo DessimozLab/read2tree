f72d0e89b3f47357826bb94ebdb2f714ceeda047
sina <sina.majidian@gmail.com>
2024-03-30 13:57:11 +0100

    add run file

---
+++ archive/run_r2t.py
@@ -0,0 +1,16 @@
+
+
+import read2tree
+from read2tree.main import main
+from read2tree._utils import exe_name
+
+import sys
+
+print("start run_r2t 2 ")
+
+main(sys.argv[1:], exe_name=exe_name(), desc="descr")
+
+
+print("finish run_r2t  ")
+
+a=1
