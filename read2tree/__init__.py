3664aab109ad53812dfcfb1dccf27194b0ac44af
Sina Majidian <sina.majidian@gmail.com>
2024-11-10 16:03:10 -0500

    v1.5.3

--- read2tree/__init__.py
+++ read2tree/__init__.py
@@ -6,7 +6,7 @@ import os
 from pkg_resources import resource_string
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-__version__ = '1.5.2'
+__version__ = '1.5.3'
 __copyright__ = 'read2tree (C) 2017-{:d} David Dylus ' \
                 .format(date.today().year)
 
