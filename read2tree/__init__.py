f0002aeba634cb18c7a42336be2ec66c73d797f3
Sina Majidian <sina.majidian@gmail.com>
2025-09-23 10:54:03 -0400

    add minimap2 version possible issue with preset short read

--- read2tree/__init__.py
+++ read2tree/__init__.py
@@ -6,7 +6,7 @@ import os
 from pkg_resources import resource_string
 logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-__version__ = '2.0.0'
+__version__ = '2.0.1'
 __copyright__ = 'read2tree (C) 2017-{:d} David Dylus,  Adrian M. Altenhoff, Sina Majidian  ' \
                 .format(date.today().year)
 
